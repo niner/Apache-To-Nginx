niner/Apache-To-Nginx

commit f117a6a24d9ad0421987b2003f119eb39821582a

Fix missing negation in Expression stringification

lib/Apache/Config.pm
@@ -187,7 +187,7 @@ class Expression {
     }
 
     method Str() {
-        return ($.begin_anchored ?? '^' !! '') ~ @.atoms».Str.join('');
+        return ($.negated ?? '!' !! '') ~ ($.begin_anchored ?? '^' !! '') ~ @.atoms».Str.join('');
     }
 }
 

commit cd70325b90817677a567a7cec46bae79683167ab

Fix regression caused by f117a6a24d9ad0421987b2003f119eb39821582a
Regexes must not contain the negation when used in nginx config and in
various string comparisons.

lib/Apache/Config.pm
@@ -38,7 +38,7 @@ class RewriteCond is Directive {
     method canonical_host() {
         return unless $.value eq '%{HTTP_HOST}';
         return unless $.regex.negated;
-        return unless $.regex ~~ /^(.*)\$$/;
+        return unless $.regex.as-regex-string ~~ /^(.*)\$$/;
         return $0.Str.subst(/\\/, '');
     }
 
@@ -196,8 +196,12 @@ class Expression {
         )
     }
 
+    method as-regex-string() {
+        return ($.begin_anchored ?? '^' !! '') ~ @.atoms».Str.join('');
+    }
+
     method Str() {
-        return ($.negated ?? '!' !! '') ~ ($.begin_anchored ?? '^' !! '') ~ @.atoms».Str.join('');
+        return ($.negated ?? '!' !! '') ~ $.as-regex-string;
     }
 }
 

lib/Apache/Config/Converter/Nginx.pm
@@ -26,7 +26,7 @@ sub replace_variables(Str $string is copy) {
 # RewriteRule ^(.*)$ http://www.kollegger.co.at$1 [R=301,L]
 
 subset CanonicalHostCondition of Apache::Config::RewriteCond where *.canonical_host;
-subset CanonicalHostRewrite of Apache::Config::RewriteRule where *.regex.Str eq '^(.*)$';
+subset CanonicalHostRewrite of Apache::Config::RewriteRule where *.regex.as-regex-string eq '^(.*)$';
 
 multi method convert_directive(
     @directives where (
@@ -250,7 +250,7 @@ multi method convert_directive(
     my $if = Nginx::Config::If.new(
         variable   => %variable_map{$cond.value.Str},
         op         => ($cond.regex.negated ?? '!' !! '') ~ ($cond.is_case_sensitive ?? '~' !! '~*'),
-        value      => $cond.regex.Str,
+        value      => $cond.regex.as-regex-string,
     );
     my @sub = {
         temp %*vhost<if_block> = $if;
@@ -279,7 +279,7 @@ multi method convert_directive(
     $parent.add-if(Nginx::Config::If.new(
         variable   => %variable_map{$cond.value.Str},
         op         => ($cond.regex.negated ?? '!' !! '') ~ ($cond.is_case_sensitive ?? '~' !! '~*'),
-        value      => $cond.regex.Str,
+        value      => $cond.regex.as-regex-string,
     ));
     $parent.directives.append(self.convert_directive(@directives));
     return Empty; # everything's contained in the NestedIf already